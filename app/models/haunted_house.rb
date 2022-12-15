b3903404bf26d4d0f406d27166df180ac1d3ccdc
PhoenixMomof2 <89948037+PhoenixMomof2@users.noreply.github.com>
2022-12-15 17:29:39 -0500

    commit tree d4f58877cb95d133f711b1a7003e7c1f8e313155

--- app/models/haunted_house.rb
+++ app/models/haunted_house.rb
@@ -1,1 +1,14 @@
-# Create your HauntedHouse class here
+class HauntedHouse 
+  # def change 
+  #   create_table :haunted_houses do |t|
+  #     t.string :name 
+  #     t.string :location 
+  #     t.string :theme
+  #     t.float :price  
+  #     t.boolean :family_friendly 
+  #     t.datetime :opening_date 
+  #     t.datetime :closed_date 
+  #     t.text :description 
+  #   end
+  # end 
+end
